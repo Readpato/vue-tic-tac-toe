9c82a96d2be8f6ee1f65175d3b533d6c30b35f4c
Patrick Raedler <patoread@gmail.com>
2022-06-24 13:19:29 +0200

    feat: Complete grid tile
    The interaction from the parent component (GameGrid) to the child (GridTile) has been properly created

--- src/components/GridTile.vue
+++ src/components/GridTile.vue
@@ -2,22 +2,25 @@
 const props = defineProps({
   id: { type: String, required: true },
   type: { type: String, required: true },
-
 })
+const emit = defineEmits(['onTileSelect'])
 
 let isCircleSelected = $ref(false)
 let isExSelected = $ref(false)
+let isTileSelected = $ref(false)
 
 const selectTile = (type: string) => {
   if (type === 'circle')
     isCircleSelected = true
   else
     isExSelected = true
+  isTileSelected = true
+  emit('onTileSelect', props.id)
 }
 </script>
 
 <template>
-  <div :id="props.id" border="2 yellow" text-yellow @click="selectTile(props.type)">
+  <div :id="props.id" :class="{ 'pointer-events-none': isTileSelected }" border="2 yellow" text-yellow @click="selectTile(props.type)">
     <div v-if="isExSelected" w-full h-full class="i-tabler-x" />
     <div v-if="isCircleSelected" w-full h-full class="i-tabler-circle" />
   </div>
