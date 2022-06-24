13cae894ba4b3b9fec6a90f9638f80ec4bf59c3b
Patrick Raedler <patoread@gmail.com>
2022-06-24 12:41:08 +0200

    feat: Add icon depending on type
    The GridTile will now add either an ex icon or a circle icon depending on which side is going to play next

--- src/components/GameGrid.vue
+++ src/components/GameGrid.vue
@@ -2,19 +2,21 @@
 const props = defineProps({
   side: { type: String, required: true, default: '' },
 })
+
+const sideType = 'circle'
 </script>
 
 <template>
   <span text="xl yellow center">Turn: {{ props.side }}</span>
   <div grid grid-cols-3 grid-rows-3 p="2" h="xs" md="h-sm" lg="h-md">
-    <GridTile id="1" />
-    <GridTile id="2" />
-    <GridTile id="3" />
-    <GridTile id="4" />
-    <GridTile id="5" />
-    <GridTile id="6" />
-    <GridTile id="7" />
-    <GridTile id="8" />
-    <GridTile id="9" />
+    <GridTile id="1" :type="sideType" />
+    <GridTile id="2" :type="sideType" />
+    <GridTile id="3" :type="sideType" />
+    <GridTile id="4" :type="sideType" />
+    <GridTile id="5" :type="sideType" />
+    <GridTile id="6" :type="sideType" />
+    <GridTile id="7" :type="sideType" />
+    <GridTile id="8" :type="sideType" />
+    <GridTile id="9" :type="sideType" />
   </div>
 </template>
--- src/components/GridTile.vue
+++ src/components/GridTile.vue
@@ -1,12 +1,24 @@
 <script setup lang="ts">
 const props = defineProps({
   id: { type: String, required: true },
+  type: { type: String, required: true },
+
 })
+
+let isCircleSelected = $ref(false)
+let isExSelected = $ref(false)
+
+const selectTile = (type: string) => {
+  if (type === 'circle')
+    isCircleSelected = true
+  else
+    isExSelected = true
+}
 </script>
 
 <template>
-  <div :id="props.id" border="2 yellow" text-yellow>
-    <div hidden w-full h-full class="i-tabler-x" />
-    <div hidden w-full h-full class="i-tabler-circle" />
+  <div :id="props.id" border="2 yellow" text-yellow @click="selectTile(props.type)">
+    <div v-if="isExSelected" w-full h-full class="i-tabler-x" />
+    <div v-if="isCircleSelected" w-full h-full class="i-tabler-circle" />
   </div>
 </template>
