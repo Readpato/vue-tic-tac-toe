775f7c90eb757675f6c61fc330ec0898f03953b0
Patrick Raedler <patoread@gmail.com>
2022-06-25 15:59:40 +0200

    refactor: Add draw possibility and change end text
    The draw possibility has been added which will allow the user to end the game when all the tiles have been selected. The endgame text has also been updated to reflect all the possibilites

--- src/components/GameGrid.vue
+++ src/components/GameGrid.vue
@@ -5,17 +5,22 @@ const props = defineProps({
 })
 
 // <-- Variables -->
-let sideType = $ref(props.side)
-const circleTilesArray: number[] = []
-const exTilesArray: number[] = []
 let hasCircleWon = $ref(false)
 let hasExWon = $ref(false)
+let isGameEnded = $ref(false)
+let isGameDraw = $ref(false)
+let sideType = $ref(props.side)
+const maximumAmountOfTilesSelected = 9
+const minimumAmountOfTilesToWin = 3
+const foundNumberAmountToWin = 3
+const circleTilesArray: number[] = []
+const exTilesArray: number[] = []
 const winCombination = [
   [1, 2, 3],
   [4, 5, 6],
   [7, 8, 9],
   [1, 4, 7],
-  [2, 5, 6],
+  [2, 5, 8],
   [3, 6, 9],
   [1, 5, 9],
   [7, 5, 3],
@@ -27,6 +32,9 @@ const showTurnIcon = computed(() => {
     return true
   else
     return false
+})
+const endGameText = computed(() => {
+  return isGameEnded && isGameDraw ? 'Draw!' : 'The winner of the game is: '
 })
 
 // <-- Functions -->
@@ -44,29 +52,39 @@ const addSelectedTile = (type: string, id: string) => {
     exTilesArray.push(numberId)
 }
 const calculateWin = (type: string) => {
-  if (circleTilesArray.length <= 2 && exTilesArray.length <= 2)
+  const combinedLengthOfArrays = circleTilesArray.length + exTilesArray.length
+  if (combinedLengthOfArrays === maximumAmountOfTilesSelected) {
+    isGameDraw = true
+    isGameEnded = true
     return
-  winCombination.forEach((winArray) => {
-    let foundNumber = 0
-    if (foundNumber === 3)
-      return
-    if (sideType === 'circle') {
-      circleTilesArray.forEach((element) => {
-        if (winArray.includes(element))
-          foundNumber++
-        if (foundNumber === 3)
-          hasCircleWon = true
-      })
-    }
-    else {
-      exTilesArray.forEach((element) => {
-        if (winArray.includes(element))
-          foundNumber++
-        if (foundNumber === 3)
-          hasExWon = true
-      })
-    }
-  })
+  }
+  if (circleTilesArray.length >= minimumAmountOfTilesToWin || exTilesArray.length >= minimumAmountOfTilesToWin) {
+    winCombination.forEach((winArray) => {
+      let foundNumber = 0
+      if (foundNumber === minimumAmountOfTilesToWin)
+        return
+      if (type === 'circle') {
+        circleTilesArray.forEach((element) => {
+          if (winArray.includes(element))
+            foundNumber++
+          if (foundNumber === foundNumberAmountToWin) {
+            hasCircleWon = true
+            isGameEnded = true
+          }
+        })
+      }
+      else {
+        exTilesArray.forEach((element) => {
+          if (winArray.includes(element))
+            foundNumber++
+          if (foundNumber === foundNumberAmountToWin) {
+            hasExWon = true
+            isGameEnded = true
+          }
+        })
+      }
+    })
+  }
 }
 const handleSelection = (id: string) => {
   addSelectedTile(sideType, id)
@@ -76,20 +94,27 @@ const handleSelection = (id: string) => {
 </script>
 
 <template>
-  <div flex justify-center items-center flex-row text-yellow space-x-1>
-    <span text="xl center">Turn: </span>
-    <div v-if="!showTurnIcon" w-8 h-8 class="i-tabler-x" />
-    <div v-if="showTurnIcon" w-8 h-8 class="i-tabler-circle" />
+  <div v-if="!isGameEnded">
+    <div flex justify-center items-center flex-row text-yellow space-x-1>
+      <span text="xl center">Turn: </span>
+      <div v-if="!showTurnIcon" w-8 h-8 class="i-tabler-x" />
+      <div v-if="showTurnIcon" w-8 h-8 class="i-tabler-circle" />
+    </div>
+    <div grid grid-cols-3 grid-rows-3 p="2" h="xs" md="h-sm" lg="h-md">
+      <GridTile id="1" :type="sideType" @on-tile-select="handleSelection" />
+      <GridTile id="2" :type="sideType" @on-tile-select="handleSelection" />
+      <GridTile id="3" :type="sideType" @on-tile-select="handleSelection" />
+      <GridTile id="4" :type="sideType" @on-tile-select="handleSelection" />
+      <GridTile id="5" :type="sideType" @on-tile-select="handleSelection" />
+      <GridTile id="6" :type="sideType" @on-tile-select="handleSelection" />
+      <GridTile id="7" :type="sideType" @on-tile-select="handleSelection" />
+      <GridTile id="8" :type="sideType" @on-tile-select="handleSelection" />
+      <GridTile id="9" :type="sideType" @on-tile-select="handleSelection" />
+    </div>
   </div>
-  <div grid grid-cols-3 grid-rows-3 p="2" h="xs" md="h-sm" lg="h-md">
-    <GridTile id="1" :type="sideType" @on-tile-select="handleSelection" />
-    <GridTile id="2" :type="sideType" @on-tile-select="handleSelection" />
-    <GridTile id="3" :type="sideType" @on-tile-select="handleSelection" />
-    <GridTile id="4" :type="sideType" @on-tile-select="handleSelection" />
-    <GridTile id="5" :type="sideType" @on-tile-select="handleSelection" />
-    <GridTile id="6" :type="sideType" @on-tile-select="handleSelection" />
-    <GridTile id="7" :type="sideType" @on-tile-select="handleSelection" />
-    <GridTile id="8" :type="sideType" @on-tile-select="handleSelection" />
-    <GridTile id="9" :type="sideType" @on-tile-select="handleSelection" />
+  <div v-if="isGameEnded" flex justify-center items-center flex-row text-yellow space-x-1>
+    <span text="xl center">{{ endGameText }}</span>
+    <div v-if="hasExWon" w-8 h-8 class="i-tabler-x" />
+    <div v-if="hasCircleWon" w-8 h-8 class="i-tabler-circle" />
   </div>
 </template>
