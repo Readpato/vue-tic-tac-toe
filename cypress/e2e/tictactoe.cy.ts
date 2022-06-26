c7118ae124410f9547cdca2f0f849bc6f56299a7
Patrick Raedler <patoread@gmail.com>
2022-06-26 19:21:29 +0200

    test: Create test for index page
    The E2E tests belonging to the index page have been created with Cypress

--- cypress/e2e/tictactoe.cy.ts
+++ cypress/e2e/tictactoe.cy.ts
@@ -1,7 +1,18 @@
 /// <reference types="cypress" />
 
-describe('empty spec', () => {
-  it('passes', () => {
+describe('Tic Tac toe', () => {
+  it('Assures that the index page has been loaded correctly', () => {
     cy.visit('/')
+    cy.get('[data-cy=main-title]').should('exist').and('be.visible')
+    cy.get('[data-cy=main-title]').should('contain.text', 'Tic Tac Toe')
+    cy.get('[data-cy=span-choose-side').should('exist').and('be.visible')
+    cy.get('[data-cy=span-choose-side').should('contain.text', 'Choose a side:')
+    cy.get('[data-cy=start-game-button-x] > div').should('have.class', 'i-tabler-x')
+    cy.get('[data-cy=start-game-button-circle] > div').should('have.class', 'i-tabler-circle')
+    cy.get('[data-cy=game-grid]').should('not.exist')
+  })
+  it('Start game on X side and assures that it can win', () => {
+    cy.get('[data-cy=start-game-button-x]').click()
+    cy.get('[data-cy=start-game-modal]').should('not.exist')
   })
 })
